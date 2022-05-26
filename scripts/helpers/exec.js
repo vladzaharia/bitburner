945d145d2f7b10aba73b2037c7cb421a419a0952
Vlad Zaharia <hey@vlad.gg>
2022-05-25 23:57:50 -0700

    Minor updates

--- scripts/helpers/exec.js
+++ scripts/helpers/exec.js
@@ -1,3 +1,12 @@
+/** @param { import("../../lib/NetscriptDefinition").NS } ns */
+export async function main(ns) {
+	if (ns.args.length < 2) {
+		throw "Function must be called with hostname and filename, threads and args optional";
+	}
+
+    await exec(ns, ns.args[0], ns.args[1], ns.args[2] || 1, ... (ns.args.splice(3) || []));
+}
+
 /** 
  * @param { import("../../lib/NetscriptDefinition").NS } ns
  * @param {string} hostname
--- scripts/helpers/scp.js
+++ scripts/helpers/scp.js
@@ -1,3 +1,12 @@
+/** @param { import("../../lib/NetscriptDefinition").NS } ns */
+export async function main(ns) {
+	if (ns.args.length < 2) {
+		throw "Function must be called with hostname and filename";
+	}
+
+    await scp(ns, ns.args[0], ns.args[1]);
+}
+
 /** 
  * @param { import("../../lib/NetscriptDefinition").NS } ns
  * @param {string} hostname
