531936160faa6c3077b39f835ffcd18a88d8c5e1
Vlad Zaharia <hey@vlad.gg>
2022-05-26 23:53:30 +0000

    Add tools and delete old scripts

--- src/tools/scp-exec.ts
+++ src/tools/scp-exec.ts
@@ -1,3 +1,4 @@
+import { NS } from "Netscript";
 import { getControlServers, getRootedHosts, getPersonalServers, getWorkerServers } from "/helpers/discover.js";
 import { exec } from "/helpers/exec.js";
 import { scp } from "/helpers/scp.js";
@@ -5,7 +6,7 @@ import { scp } from "/helpers/scp.js";
 /** 
  * @param {NS} ns
  */
-export async function main(ns) {
+export async function main(ns: NS) {
 	let hostnames = await getPersonalServers(ns);
 	const hackableHosts = await getRootedHosts(ns);
 	let threads = 0;
@@ -16,7 +17,7 @@ export async function main(ns) {
 
 	// Filter based on arg[0] if provided
 	if (ns.args.length > 0) {
-		const hostnameArg = ns.args[0];
+		const hostnameArg = ns.args[0] as string;
 
 		if (hostnameArg === "control") {
 			hostnames = await getControlServers(ns);
@@ -31,18 +32,18 @@ export async function main(ns) {
 
 		// Execute alternative script
 		if (ns.args.length > 1) {
-			filename = ns.args[1];
+			filename = ns.args[1] as string;
             args = [];
 			ns.print(`[scp-exec] Executing alternative script: ${filename}`);
 
 			// Change number of threads
 			if (ns.args.length > 2) {
-				threads = ns.args[2];
+				threads = ns.args[2] as number;
 				ns.print(`[scp-exec] Executing alternative thread count: ${threads}`);
 
 				// Send rest of arguments to the script
 				if (ns.args.length > 3) {
-					args = ns.args.slice(3);
+					args = ns.args.slice(3) as string[];
 				}
 			}
 		}		
--- src/tools/sell-server.ts
+++ src/tools/sell-server.ts
@@ -1,12 +1,14 @@
+import { NS } from "Netscript";
+
 /** 
  * @param {NS} ns
  */
-export async function main(ns) {
+export async function main(ns: NS) {
     if (ns.args.length === 0) {
         throw "Function must be called with servers to sell!"
     }
     
-    const purchasedServers = ns.args;
+    const purchasedServers = ns.args as string[];
 
 	for (let i = 0; i < purchasedServers.length; i++) {
 		await ns.killall(purchasedServers[i]);
