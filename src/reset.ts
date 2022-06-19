359870ab4d77878019a48c3cc80b115bf5650182
Vlad Zaharia <hey@vlad.gg>
2022-06-19 15:31:23 -0700

    Update reset script to target specific BNs

--- src/reset.ts
+++ src/reset.ts
@@ -3,6 +3,9 @@ import { NS } from "Netscript";
 import { AugmentationStore } from "/_internal/classes/store/augmentations.js";
 import { sleep } from "/helpers/sleep.js";
 
+/** Order to run bitnodes, will try to fill. */
+const BITNODE_ORDER = [1, 4];
+
 /**
  * Automatically purchase and install augmentations, or if endgame is available, try destroying w0r1d_d43m0n.
  * @category Executable
@@ -19,18 +22,52 @@ export async function main(ns: NS) {
     ns.disableLog("ALL");
     ns.clearLog();
 
-    const augmentationStore = new AugmentationStore(ns);
-    let augmentationToPurchase = augmentationStore.getAugmentationToPurchase();
+    if (ns.singularity.getOwnedAugmentations().includes("The Red Pill")) {
+        ns.print(
+            `[reset] Red Pill owned, checking if we can destroy w0r1d_d43m0n`
+        );
+        const level = ns.getHackingLevel();
+        const reqLevel = ns.getServerRequiredHackingLevel("w0r1d_d43m0n");
 
-    ns.print(`[reset] Purchasing augmentations...`);
-    while (augmentationToPurchase) {
-        augmentationStore.purchase(augmentationToPurchase);
-        augmentationToPurchase = augmentationStore.getAugmentationToPurchase();
+        if (level > reqLevel) {
+            const available = ns
+                .getOwnedSourceFiles()
+                .filter((os) => os.lvl < 3);
+            const priority = available.filter((os) =>
+                BITNODE_ORDER.includes(os.n)
+            );
 
-        await sleep(ns, 2 * 1000);
+            let bitnode = available[0];
+
+            if (priority.length > 0) {
+                bitnode = priority[0];
+            }
+
+            ns.print(
+                `[reset] Destroying w0r1d_d43m0n and hopping to BN${bitnode.n} (${bitnode.lvl}/3)`
+            );
+            ns.singularity.destroyW0r1dD43m0n(bitnode.n, "init.js");
+        } else {
+            ns.print(
+                `[reset] Do not have enough hacking level ${level}/${reqLevel}`
+            );
+        }
+    } else {
+        const augmentationStore = new AugmentationStore(ns);
+        let augmentationToPurchase =
+            augmentationStore.getAugmentationToPurchase();
+
+        ns.print(`[reset] Purchasing augmentations...`);
+        while (augmentationToPurchase) {
+            augmentationStore.purchase(augmentationToPurchase);
+            augmentationToPurchase =
+                augmentationStore.getAugmentationToPurchase();
+
+            await sleep(ns, 2 * 1000);
+        }
+
+        // Install augmentations and run init.js script
+        ns.print(`[reset] Installing augmentations...`);
+        ns.singularity.installAugmentations("init.js");
     }
-
-    // Install augmentations and run init.js script
-    ns.print(`[reset] Installing augmentations...`);
-    ns.singularity.installAugmentations("init.js");
 }
